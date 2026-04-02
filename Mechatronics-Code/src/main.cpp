28874dc72bcae3fea689191cb082096dbcebf759
Ryan Goldstein <ryan.goldstein212@gmail.com>
2026-04-02 16:32:45 -0400

    Servo and Serial monitor test code added

--- Mechatronics-Code/src/main.cpp
+++ Mechatronics-Code/src/main.cpp
@@ -1,23 +1,37 @@
 #include <Arduino.h>
+#include <Servo.h>
+
+#define servoPin 30
+
+Servo transitionHoleservo;
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  transitionHoleservo.attach(servoPin);
   pinMode(13, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(13, 1);
+  transitionHoleservo.write(90);
+  Serial.print("Hello World");
   delay(1000);
+
   digitalWrite(13, 0);
+  transitionHoleservo.write(0);
+  Serial.print("Goodbye World");
   delay(1000);
+  
+  
 }
 
 // put function definitions here:
 int myFunction(int x, int y) {
   return x + y;
 }
+
