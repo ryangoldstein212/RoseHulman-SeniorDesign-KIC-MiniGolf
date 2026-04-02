38e09c277cbd646f2235459fd9092107d53ae633
Ryan Goldstein <ryan.goldstein212@gmail.com>
2026-04-02 15:12:25 -0400

    testing platform.io with a commit

---
+++ Mechatronics-Code/src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13, 1);
+  delay(500);
+  digitalWrite(13, 0);
+  delay(500);
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
