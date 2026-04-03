7725fea92b3cf55977f57ce331413f05e26042f2
Ryan Goldstein <ryan.goldstein212@gmail.com>
2026-04-03 16:51:58 -0400

    began writing code based on ryan hand written diagram

--- Mechatronics-Code/src/main.cpp
+++ Mechatronics-Code/src/main.cpp
@@ -1,32 +1,38 @@
 #include <Arduino.h>
 #include <Servo.h>
 
-#define servoPin 30
+#define servoPin 9
+#define transitionHole_sensorPin 1
 
-Servo transitionHoleservo;
+int tranistionHole_closed = 90;
+int tranistionHole_open = 150;
+
+Servo transitionHole_servo;
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  transitionHoleservo.attach(servoPin);
+  transitionHole_servo.attach(servoPin);
   pinMode(13, OUTPUT);
+
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(13, 1);
-  transitionHoleservo.write(90);
-  Serial.print("Hello World");
-  delay(1000);
 
-  digitalWrite(13, 0);
-  transitionHoleservo.write(0);
-  Serial.print("Goodbye World");
-  delay(1000);
   
+  
+  digitalWrite(13, 1);
+  transitionHole_servo.write(tranistionHole_closed);
+  /*
+  int transitionHole_sensorValue = analogRead(transitionHole_sensorPin);
+  Serial.print(transitionHole_sensorValue);
+  Serial.print("\n       ");
+  */
+
   
 }
 
@@ -35,3 +41,6 @@ int myFunction(int x, int y) {
   return x + y;
 }
 
+void detectedHandISR(){
+
+}
