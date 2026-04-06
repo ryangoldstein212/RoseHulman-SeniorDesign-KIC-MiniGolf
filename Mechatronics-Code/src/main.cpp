e63633f4ca13cf396feac0384071d387bad6fb36
Ryan Goldstein <ryan.goldstein212@gmail.com>
2026-04-06 16:48:28 -0400

    added globals, started commeting code strucutre for functionality

--- Mechatronics-Code/src/main.cpp
+++ Mechatronics-Code/src/main.cpp
@@ -1,13 +1,38 @@
 #include <Arduino.h>
 #include <Servo.h>
 
+// Digital Pin definitions
 #define servoPin 9
-#define transitionHole_sensorPin 1
 
+// Analog Pin definitions
+#define transitionHole_sensorPin 0
+#define pointValue_15_sensorPin 1 // analog port for sensors 1-5
+#define pointValue_610_sensorPin 2 // analog port for sensors 6-10
+
+// Global variable setting
 int tranistionHole_closed = 90;
 int tranistionHole_open = 150;
 int lightSensor_threshold = 50;
+int point_threshold = 20
+long pointValue_1_threshold = 325;
+long pointValue_2_threshold = 415;
+long pointValue_3_threshold = 620;
+long pointValue_4_threshold = 690;
+long pointValue_5_threshold = 830;
 
+bool detectedHand = false;
+bool finalHole = false;
+
+// counter and timer setting
+int handDetection_count = 0;
+int playerScore = 0;
+int pointValue_1 = 25;
+int pointValue_2 = 50;
+int pointValue_3 = 75;
+int pointValue_4 = 100;
+int pointValue_5 = 125;
+
+// Transition hole servo object
 Servo transitionHole_servo;
 
 // put function declarations here:
@@ -16,23 +41,45 @@ int myFunction(int, int);
 void setup() {
   // put your setup code here, to run once:
   transitionHole_servo.attach(servoPin);
-  pinMode(13, OUTPUT);
 
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  
-  
-  digitalWrite(13, 1);
-  transitionHole_servo.write(tranistionHole_closed);
   /*
+  // Plinko and upper step system are on only
+  
+
+  // Upper Step into Transition Hole
+  if detectionCount >= 2 {
+    detectedHand = true;
+  }
+
+  if detectedHand = true {
+    // When a hand is detected, open transition hole and then set timer for 30 sec before closing
+    transitionHole_servo.write(transitionHole_open);
+  }
+
+  if detectedHand_time = 30 {
+    // After 30 seconds has passed, close servo and end detectedHand state
+    transitionHole_servo.write(transitionHole_closed);
+    detectedHand = false;
+  }
+  // Lower Step
+  if finalHole == true {
+
+  }
+  // End
+
+  */
+
+  //testing
+  transitionHole_servo.write(tranistionHole_open);
+  
   int transitionHole_sensorValue = analogRead(transitionHole_sensorPin);
   Serial.print(transitionHole_sensorValue);
   Serial.print("\n       ");
-  */
+  
 
   
 }
@@ -42,6 +89,14 @@ int myFunction(int x, int y) {
   return x + y;
 }
 
-void detectedHandISR(){
+long countScore(int ){
 
+}
+
+void detectedHandISR(){
+  // when light beam is broken, incriment detection count
+}
+
+void finalHoleISR(){
+  // 
 }
